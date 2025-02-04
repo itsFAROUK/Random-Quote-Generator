97b1b0824bb29e2ceeb4c1fe7b0953a81c2584cf
Farouk <bouazizfarouk3@gmail.com>
2025-02-04 21:31:49 +0100

    small improvements

--- script.js
+++ script.js
@@ -18,17 +18,35 @@ function randomQuote() {
         headers: {
             "X-Api-key": "SmOtameb9wLcsm0womwmnA==MhzqQvOfBIIcvzKz"
         }
-    }).then(res => res.json()).then(result => {
-        quoteText.innerText = result[0].quote
-        author.innerText = result[0].author
+    })
+    .then(res => {
+        if (!res.ok) throw new Error("Failed to fetch quote")
+        return res.json()
+    })
+    .then(result => {
+        quoteText.innerText = result[0].quote || "No quote available."
+        author.innerText = result[0].author || "Unknown"
+    })
+    .catch(error => {
+        quoteText.innerText = "Failed to load quote. Try again!"
+        author.innerText = ""
+        console.error(error)
+        console.log(error)
+    })
+    .finally(() => {
         newQuoteBtn.innerText = "New Quote"
         newQuoteBtn.classList.remove("loading")
     })
 }
 
 function hearQuote() {
-    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${author.innerText}`)
-    speechSynthesis.speak(utterance)
+    let utterance = new SpeechSynthesisUtterance(`${quoteText.innerText} by ${author.innerText}`);
+    speechSynthesis.onvoiceschanged = () => {
+        let voices = speechSynthesis.getVoices();
+        utterance.voice = voices.find(voice => voice.name.includes("Microsoft Steffan Online (Natural) - English (United States)")) || voices[0];
+        speechSynthesis.speak(utterance);
+    };
+
 }
 
 function copyQuote() {
@@ -36,7 +54,7 @@ function copyQuote() {
 }
 
 function shareOnXTwitter() {
-    let tweetUrl =`https://twitter.com/intent/tweet/?url=${quoteText.innerText}`
+    let tweetUrl =`https://twitter.com/intent/tweet?text=${encodeURIComponent(quoteText.innerText + "-" + author.innerText)}`
     window.open(tweetUrl, "_blank")
 }
 
